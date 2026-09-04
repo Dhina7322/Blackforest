89f25d4a235fd6edc9d0327c0b4af6134f3b215c
Dhina7322 <dhina7322@gmail.com>
2026-09-04 11:37:25 +0530

    Add root src/server.js and server.js bridges to resolve Render Start Command 'node src/server.js'

---
+++ server.js
@@ -0,0 +1,3 @@
+// Blackforest Holidays - Root entry bridge
+// Resolves commands like 'node server.js' to the main server implementation
+module.exports = require('./server/src/server.js');
---
+++ src/server.js
@@ -0,0 +1,3 @@
+// Blackforest Holidays - PaaS entry point bridge
+// Resolves commands like 'node src/server.js' to the main server implementation
+module.exports = require('../server/src/server.js');
